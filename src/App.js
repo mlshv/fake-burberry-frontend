34dd221b5eb87119ea3c5890833ff0245031436b
Mikhail Malyshev <mlshv42@ya.ru>
2017-08-12 10:34:17 +0300

    fix: move i18n provider into the app component

--- src/App.js
+++ src/App.js
@@ -1,19 +1,26 @@
 import React from 'react'
 import { Helmet } from 'react-helmet'
+import { IntlProvider } from 'react-intl'
+import { addLocaleData } from 'react-intl'
+import ruLocaleData from 'react-intl/locale-data/ru'
 import Header from './Header'
 import Product from './Product'
 import Footer from './Footer'
 
+addLocaleData(ruLocaleData)
+
 function App() {
   return (
-    <div>
-      <Helmet>
-        <title>Burberry</title>
-      </Helmet>
-      <Header />
-      <Product />
-      <Footer />
-    </div>
+    <IntlProvider locale="ru">
+      <div>
+        <Helmet>
+          <title>Burberry</title>
+        </Helmet>
+        <Header />
+        <Product />
+        <Footer />
+      </div>
+    </IntlProvider>
   )
 }
 
--- src/index.js
+++ src/index.js
@@ -1,20 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { IntlProvider } from 'react-intl'
-import { addLocaleData } from 'react-intl'
-import ruLocaleData from 'react-intl/locale-data/ru'
 import 'normalize.css'
 import 'flexboxgrid2'
 import './index.css'
 import App from './App'
 import registerServiceWorker from './registerServiceWorker'
 
-addLocaleData(ruLocaleData)
-
-ReactDOM.render(
-  <IntlProvider locale="ru">
-    <App />
-  </IntlProvider>,
-  document.getElementById('root')
-)
+ReactDOM.render(<App />, document.getElementById('root'))
 registerServiceWorker()
