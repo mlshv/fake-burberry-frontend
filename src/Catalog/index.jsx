103ef8e023caa61306e293e4a7c78a805718b8f2
Mikhail Malyshev <mlshv42@ya.ru>
2017-08-15 21:40:36 +0300

    feat: add product list with cards

--- src/Catalog/index.jsx
+++ src/Catalog/index.jsx
@@ -1,6 +1,8 @@
-import React from 'react';
+import React, { Component } from 'react'; // eslint-disable-line
 import styled from 'styled-components';
 import Jumbotron from './Jumbotron';
+import Card from './Card';
+import products from './products.json';
 
 const CategoryTitle = styled.h2`
   margin-top: 2rem;
@@ -13,5 +15,18 @@ export default () =>
     <Jumbotron />
     <div className="container">
       <CategoryTitle>Heritage Trench Coats</CategoryTitle>
+      <div className="row">
+        {products.map(product =>
+          (<div className="col-xs-6">
+            <Card
+              image={product.photo}
+              name={product.name}
+              tag={product.tag}
+              colors={product.colors}
+              price={product.price}
+            />
+          </div>),
+        )}
+      </div>
     </div>
   </div>);
