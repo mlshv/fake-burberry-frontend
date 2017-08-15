5eb6de4860198cfd9fec41dc1aab97d72d33be42
Mikhail Malyshev <mlshv42@ya.ru>
2017-08-15 18:37:07 +0300

    feat: add navigation to header

--- src/Header/index.jsx
+++ src/Header/index.jsx
@@ -1,33 +1,34 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import styled from 'styled-components';
-import logo from './assets/logo.svg';
+import logo from '../assets/logo.svg';
+import Navigation from './Navigation';
 
 const Header = styled.header`
-  padding: 1rem;
   border-bottom: 1px solid #c6c6c6;
-
-  @media screen and (min-width: 48rem) {
-    padding: 1.5rem;
-  }
   @media screen and (min-width: 62rem) {
-    padding: 2rem;
     border-bottom: none;
   }
 `;
 
 const Logo = styled.img`
   display: block;
-  margin: auto;
-  height: 10px;
+  height: 12px;
+  margin: 1rem auto;
+  @media screen and (min-width: 48rem) {
+    margin: 1.5rem auto;
+  }
   @media screen and (min-width: 62rem) {
-    height: 16px;
+    margin: 2rem auto;
   }
 `;
 
 export default () =>
-  (<Header>
-    <Link to="/">
-      <Logo alt="Logo" src={logo} />
-    </Link>
-  </Header>);
+  (<div>
+    <Header>
+      <Link to="/">
+        <Logo alt="Logo" src={logo} />
+      </Link>
+      <Navigation />
+    </Header>
+  </div>);
