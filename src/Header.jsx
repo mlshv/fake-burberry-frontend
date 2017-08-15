00f0b2c8f08a5b865a4b54bd967718c345b609d1
Mikhail Malyshev <mlshv42@ya.ru>
2017-08-15 23:01:41 +0300

    fix: set proper eslint running command and fix linted errors

---
+++ src/Header.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import styled from 'styled-components';
+import logo from './assets/logo.svg';
+
+const Header = styled.header`
+  padding: 1rem;
+  border-bottom: 1px solid #c6c6c6;
+
+  @media screen and (min-width: 48rem) {
+    padding: 1.5rem;
+  }
+  @media screen and (min-width: 62rem) {
+    padding: 2rem;
+    border-bottom: none;
+  }
+`;
+
+const Logo = styled.img`
+  display: block;
+  margin: auto;
+  height: 10px;
+  @media screen and (min-width: 62rem) {
+    height: 16px;
+  }
+`;
+
+export default () =>
+  (<Header>
+    <a href="/">
+      <Logo alt="Logo" src={logo} />
+    </a>
+  </Header>);
