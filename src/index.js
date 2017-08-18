7fb7a6509d0cc1e0d3d533f3af0264f1959a5845
Mikhail Malyshev <mlshv42@ya.ru>
2017-08-19 02:08:08 +0300

    feat: upgrade flexboxgrid2

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import 'normalize.css';
-import 'flexboxgrid2/dist/flexboxgrid.css';
+import 'flexboxgrid2/flexboxgrid2.css';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
