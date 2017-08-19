49b6d2f5019847a37f29363da2144662cb1b5082
Mikhail Malyshev <mlshv42@ya.ru>
2017-08-19 14:55:10 +0300

    feat: implement active category navlink highlighting

--- src/Header/Navigation.jsx
+++ src/Header/Navigation.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import styled, { css } from 'styled-components';
+import { NavLink } from 'react-router-dom';
+import styled from 'styled-components';
 
 const Navigation = styled.nav`
   display: none;
@@ -9,7 +10,7 @@ const Navigation = styled.nav`
   }
 `;
 
-const Link = styled.a`
+const Link = styled(NavLink)`
   padding: 1rem;
   font-size: .75rem;
   font-family: Raleway, 'Helvetica Neue', Helvetica, Arial;
@@ -19,22 +20,18 @@ const Link = styled.a`
   line-height: 1.34;
   letter-spacing: 1.5px;
   color: #999999;
-  ${props =>
-    props.active &&
-    css`
+  &.active {
     padding-bottom: 1px;
     color: #171717;
     border-bottom: 1px solid #171717;
-  `};
+  }
 `;
 
 export default () =>
   (<Navigation>
-    <Link href="#">Women</Link>
-    <Link active href="#">
-      Men
-    </Link>
-    <Link href="#">Children</Link>
-    <Link href="#">Beauty</Link>
-    <Link href="#">Experience</Link>
+    <Link to="/women">Women</Link>
+    <Link to="/men">Men</Link>
+    <Link to="/children">Children</Link>
+    <Link to="/beauty">Beauty</Link>
+    <Link to="/experience">Experience</Link>
   </Navigation>);
