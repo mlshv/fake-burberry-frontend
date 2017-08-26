181f9eb303a5338275da229a5291063ae64680b4
Mikhail Malyshev <mlshv42@ya.ru>
2017-08-26 13:39:43 +0300

    feat: implement location selector using <select>

--- src/Header/LocationSelector.jsx
+++ src/Header/LocationSelector.jsx
@@ -1,8 +1,13 @@
 import React, { Component } from 'react';
-import styled, { css } from 'styled-components';
+import styled from 'styled-components';
 import arrowIcon from '../assets/arrow.svg';
 
-const LocationSelectorStyled = styled.button`
+const Wrapper = styled.div`
+  display: flex;
+  align-items: baseline;
+`;
+
+const LocationSelectorStyled = styled.select`
   display: inline-block;
   padding: 1.5rem 0;
   font-size: .75rem;
@@ -12,61 +17,38 @@ const LocationSelectorStyled = styled.button`
   color: #999;
   background: transparent;
 
+  -webkit-appearance: none;
+  -moz-appearance: none;
+  text-indent: 1px;
+  text-overflow: '';
+  ::-ms-expand {
+    display: none;
+  }
+
   @media screen and (min-width: 48rem) {
     margin: 0;
     font-size: .75rem;
-    font-weight: 600;
+    font-weight: 500;
     line-height: 1.4;
   }
 
   @media screen and (min-width: 62rem) {
     padding: 2rem 0;
   }
-
-  ::after {
-    display: inline-block;
-    width: 12px;
-    height: 6px;
-    margin-left: .5rem;
-    background: url(${arrowIcon});
-    background-size: cover;
-    content: " ";
-    ${props =>
-    props.active &&
-      css`
-      transform: rotate(180deg);
-    `};
-  }
 `;
 
-const Locations = styled.div`
-  display: flex;
-  flex-direction: column;
-  position: absolute;
-  white-space: normal;
-  font-family: 'Raleway', 'Helvetica Neue', Helvetica, Arial;
-  font-size: .75rem;
-  background: #fff;
-`;
-
-const Location = styled.button`
-  flex-basis: 100%;
-  padding: 1rem;
-  border: 0;
-  background: none;
-  font-family: 'Raleway', 'Helvetica Neue', Helvetica, Arial;
-  color: #999;
+const ArrowIcon = styled.span`
+  width: 12px;
+  height: 6px;
+  margin-top: 1.5rem;
+  margin-left: .5rem;
+  background: url(${arrowIcon});
+  background-size: cover;
+  content: " ";
 `;
 
 class LocationSelector extends Component {
-  constructor() {
-    super();
-    this.toggle = this.toggle.bind(this);
-    this.select = this.select.bind(this);
-  }
-
   state = {
-    active: false,
     locations: [
       {
         name: 'United Kingdom',
@@ -81,42 +63,20 @@ class LocationSelector extends Component {
         currency: '₽',
       },
     ],
-    locationSelected: 0,
   };
 
-  toggle() {
-    this.setState(state => ({
-      active: !state.active,
-    }));
-  }
-
-  select(index) {
-    this.setState({
-      locationSelected: index,
-      active: false,
-    });
-  }
-
   render() {
-    const location = this.state.locations[this.state.locationSelected];
     return (
-      <div>
+      <Wrapper>
         <LocationSelectorStyled active={this.state.active} onClick={this.toggle}>
-          Shopping in: {location.name} ({location.currency}){' '}
+          {this.state.locations.map(l =>
+            (<option>
+              Shopping in: {l.name} ({l.currency}){' '}
+            </option>),
+          )}
         </LocationSelectorStyled>
-        {this.state.active &&
-          <Locations>
-            {this.state.locations.map((l, index) =>
-              (<Location
-                onClick={() => {
-                  this.select(index);
-                }}
-              >
-                {l.name} ({l.currency})
-              </Location>),
-            )}
-          </Locations>}
-      </div>
+        <ArrowIcon />
+      </Wrapper>
     );
   }
 }
