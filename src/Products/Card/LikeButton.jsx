6c811c78e0ed6fce35cbde6951060d4ff55c8b34
Mikhail Malyshev <mlshv42@ya.ru>
2017-08-29 13:21:18 +0300

    chore: extract svg heart as common component

--- src/Products/Card/LikeButton.jsx
+++ src/Products/Card/LikeButton.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import styled from 'styled-components';
+import HeartIcon from '../../common/HeartIcon';
 
 const LikeButtonStyled = styled.button`
   padding: 0;
@@ -28,35 +29,7 @@ class LikeButton extends Component {
   render() {
     return (
       <LikeButtonStyled onClick={this.toggle}>
-        <svg
-          width="16px"
-          height="15px"
-          viewBox="0 0 16 15"
-          version="1.1"
-          xmlns="http://www.w3.org/2000/svg"
-          xmlnsXlink="http://www.w3.org/1999/xlink"
-        >
-          <defs>
-            <path
-              d="M7.00147577,14 L1.1995764,7.7486402 C-0.43417144,5.94319333 -0.39228047,3.05447833 1.28335834,1.29416763 C2.85426972,-0.353302646 5.34678245,-0.44357499 7.00147577,1.11362294 C8.67711458,-0.44357499 11.1905728,-0.353302646 12.7614842,1.3393038 L12.7614842,1.3393038 C14.395232,3.0996145 14.4161775,5.96576141 12.8033751,7.7486402 L12.8033751,7.7486402 L7.00147577,14 Z"
-              id="path-1"
-            />
-          </defs>
-          <g id="Symbols" stroke="none" strokeWidth="1" fill="none" fillRule="evenodd">
-            <g id="Icon-/-Like" transform="translate(1.000000, 1.000000)">
-              <mask id="mask-2" fill="white">
-                <use xlinkHref="#path-1" />
-              </mask>
-              <use
-                id="Shape"
-                stroke="#171717"
-                fillRule="nonzero"
-                xlinkHref="#path-1"
-                {...(this.state.active ? { fill: '#171717' } : '')}
-              />
-            </g>
-          </g>
-        </svg>
+        <HeartIcon {...(this.state.active ? { fillColor: '#171717' } : '')} />
       </LikeButtonStyled>
     );
   }
